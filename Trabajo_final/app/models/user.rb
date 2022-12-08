fab4876d99373ff1912363e5b82ee635d6cca92b
Ciuel <juantvicens@gmail.com>
2022-12-08 00:20:28 -0300

    Continued with appointments ui, edit form for the admin incomplete

--- Trabajo_final/app/models/user.rb
+++ Trabajo_final/app/models/user.rb
@@ -11,9 +11,10 @@ class User < ApplicationRecord
 
 
 
+  belongs_to :branch, optional: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  belongs_to :branch, optional: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
